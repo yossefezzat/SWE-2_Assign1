2097539bdc0a29feec490714adc1150a7cf0da4f
yossefezzat <45318576+yossefezzat@users.noreply.github.com>
2019-02-25 02:03:44 +0200

    templates

---
+++ templates/navbar.php
@@ -0,0 +1,34 @@
+<?php
+   /* 
+  $username = $_SESSION['Username'];
+  $q2=$con->prepare("SELECT FullName from users where UserName='$username'");
+  $q2->execute();
+  
+  $row = $q2->fetch();
+  $name = $row['FullName'];
+  */
+?>
+
+<!-- Start Header -->
+<div class="header main-back">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="logo">
+        <h2 class="h1 logo"><a href="home.php" >Fsحny</a><i class="fas fa-seedling"></i></h2>
+      </div>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse d-flex" id="navbarSupportedContent">
+        <div class="links">
+          <ul class="navbar-nav">
+            
+          </ul>
+        </div>
+        <div class="profile">
+
+             
+        </div>
+      </div>
+    </nav>
+</div>
+<!-- End Header -->
